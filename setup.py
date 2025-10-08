e26ba89fe55832b2a8a64e396509fffef2dffef3
sakshi-mani-tripathi <sakshimani2002@gmail.com>
2025-10-08 20:55:45 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.1.0',
+    author =  'Sakshi Mani Tripathi',
+    packages = find_packages(),
+    install_requires = []
+
+)
